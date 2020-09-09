480514126f6363515a85a713a40d1e439182eb2e
simmoe <simmoe@gmail.com>
2020-09-09 08:57:45 +0200

    updated template

---
+++ svelte-basic-template/src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<main>
+	<div>
+		<h1>Digital Teknik 2020</h1>
+	</div>
+</main>
+
+<style>
+	main {
+		width:100vw;
+		height:100vh;
+		display:grid;
+		place-items:center;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+		margin-top:0;
+	}
+</style>
